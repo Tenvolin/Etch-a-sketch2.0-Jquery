a4105691b8b6f431a475766046eae0e05f5fe940
alexc <chung.alex95@gmail.com>
2016-02-18 03:09:43 -0800

    Used flexbox to implement grid system; working highlight and clear button

---
+++ project.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+	setup();
+	$(".blank").hover(function() {
+		$(this).addClass('highlight');
+		$(this).removeClass('blank');
+	})
+	
+	$("#reset").click(function() {
+		$(".highlight").addClass("blank");
+		$('.highlight').removeClass('highlight');
+	});
+	
+})
+
+
+
+/*
+jQuery.fn.extend({
+	setup: function() {
+		alert("hello!");
+	}
+})
+*/
+function setup() {
+	/*add invisible box to doc*/
+	var $box = $("<div class='blank'></div>").hide().appendTo("#container");
+	/*retrieve width value from said box*/
+	var box_width = $box.css("width");
+	box_width = parseInt(box_width);
+	/*$('<div class="content" />').css('color') is a valid alternative; see if it works in ff and ie*/
+	/*initialize default grid width*/
+	var cols = 16;
+	var total_width = box_width * cols; 
+	$("#container").css("width", total_width);
+	/*determine number of squares to throw into container*/
+	var num_squares = cols * cols;
+	for(i=0; i<num_squares; i++) {
+		$("#container").append("<div class='blank'></div>");
+	}
+}
+
+function manual_setup(n) {
+	/*add invisible box to doc*/
+	var $box = $("<div class='blank'></div>").hide().appendTo("#container");
+	/*retrieve width value from said box*/
+	var box_width = $box.css("width");
+	box_width = parseInt(box_width);
+	/*$('<div class="content" />').css('color') is a valid alternative; see if it works in ff and ie*/
+	/*initialize default grid width*/
+	var cols = n;
+	var total_width = box_width * cols; 
+	$("#container").css("width", total_width);
+	/*determine number of squares to throw into container*/
+	var num_squares = n * n;
+	for(i=0; i<num_squares; i++) {
+		$("#container").append("<div class='blank'></div>");
+	}
+}
